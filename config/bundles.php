bb898963052a01297252ffddfa4aea161637fe51
Jackivan007 <ivancucarella@gmail.com>
2025-05-28 16:39:18 +0200

    Configurar .htaccess en public/ y raíz del proyecto

--- config/bundles.php
+++ config/bundles.php
@@ -4,7 +4,10 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+
+    // Solo cargar DebugBundle si está instalado y solo en dev y test
+    class_exists(\Symfony\Bundle\DebugBundle\DebugBundle::class) ? Symfony\Bundle\DebugBundle\DebugBundle::class : null => ['dev' => true, 'test' => true],
+
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
